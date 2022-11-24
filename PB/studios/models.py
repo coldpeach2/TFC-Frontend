bb3a11686fe6013f9d07812d332603f2bd6b84f4
shauna <shauna.vronces@mail.utoronto.ca>
2022-11-24 16:29:26 -0500

    shauna Nov 24 commit

--- PB/studios/models.py
+++ PB/studios/models.py
@@ -1,10 +1,18 @@
-#from django.contrib.gis.db import models
 from django.db import models
+from django.contrib.gis.db.models import PointField
 # Create your models here.
+
+
 class Studio(models.Model):
-    name =  models.CharField(max_length=120)
-    address =  models.CharField(max_length=120)
-    #geolocation = models.PointField()
-    #postal_code = 
-    #phone_num = 
-    #images = 
+    name = models.CharField(max_length=120)
+    address = models.CharField(max_length=120)
+    geolocation = PointField(default=None)
+    postal_code = models.CharField(max_length=7)
+    phone_num = models.CharField(max_length=12)
+    images = models.ImageField(upload_to='studios')
+
+
+class Amenity(models.Model):
+    type = models.CharField(max_length=120)
+    quantity = models.IntegerField()
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
