7d404006f8019945fdfeb0e2e0a6ca67661d295e
areej <areej.negm@mail.utoronto.ca>
2022-11-25 19:06:39 -0500

    classes models works now slay

--- PB/classes/admin.py
+++ PB/classes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Classes
+# Register your models here.
+
+admin.site.register(Classes)
+
 # Register your models here.
--- PB/classes/models.py
+++ PB/classes/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from accounts.models import User
 from studios.models import Studio
-
+from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 
 class Classes(models.Model):
-    studio = models.ManyToManyField(Studio, related_name='studios_studio')
+    #studio = GenericRelation(Studio)
+    studio = models.ManyToManyField(Studio, related_name='studios_classes')
     name = models.CharField(max_length=120)
     description = models.CharField(max_length=120)
     coach = models.CharField(max_length=120)
