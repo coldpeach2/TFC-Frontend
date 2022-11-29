e43506ad87b90017981799532505f3939d97fae8
shauna <shauna.vronces@mail.utoronto.ca>
2022-11-28 21:09:43 -0500

    added urls for search views

--- PB/studios/urls.py
+++ PB/studios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ViewStudioView, StudiosForUserView, StudioClassScheduleView
+from .views import ViewStudioView, StudiosForUserView, StudioClassScheduleView, UserStudioSearch, UserClassSearch
 
 app_name = 'studios'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:studio_id>/view/', ViewStudioView.as_view()),
     #path('all/', StudiosForUserView.as_view()),
     path('<int:id>/classes/', StudioClassScheduleView.as_view()),
+    path('studio/search/', UserStudioSearch.as_view()),
+    path('classes/search/', UserClassSearch.as_view()),
 ]
