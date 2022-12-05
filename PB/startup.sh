9a1bae3e386ef1e4b95613a4340ff238b5777d7a
areej <areej.negm@mail.utoronto.ca>
2022-12-05 15:03:21 -0500

    deleted pycache and migrations

--- PB/startup.sh
+++ PB/startup.sh
@@ -1,6 +1,4 @@
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 manage.py makemigrations accounts
-python3 manage.py makemigrations studios
 python3 manage.py makemigrations
 python3 manage.py migrate
